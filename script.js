2aa2182aff76ab790a6cbc2fff71d871285b15c0
San <sruidiaz@hotmail.com>
2021-05-07 12:55:26 +0200

    Se linkeo la imagen y fondo

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
-//Seteado de imagen
+//Declarado de variables
+const memeImg = document.querySelector('img');
+let imgInput= document.getElementById('image-input')
+let colorPicked= document.querySelector('#colorPicked');
+//Seteado de imagen: la url de la imagen ingresada se agrega a la etiqueta img
 
-// let img= document.querySelector('img').src;
 
-
-
-// const getUrlImage=()=>{
-//     let image=document.querySelector('#image-input').value;
-//     img.appendChild(image)
-//     console.log(image)
-// }
 function getUrlImage()
-{
-    let image= document.getElementById('image-input').value;
-        img = document.querySelector('img');
-        imageUrl= document.createElement(image);
-
-        img.appendChild(imageUrl);
-
+{ 
+    let srcMeme = imgInput.value   
+    console.log(srcMeme)
+    const memeImg = document.querySelector('img');
+    console.log(memeImg)
+    memeImg.style.backgroundImage = `url("${srcMeme}")`;
 }
-// image.addEventListener('change', getUrlImage)
+
+imgInput.addEventListener('keyup', getUrlImage);
+
+//seteado de color de imagen: con un color picker se cambia el color de la imagen
+
+const changeBackgroundImage=()=>{
+    memeImg.style=`background-color:${colorPicked.value}`
+}
+colorPicked.addEventListener('input', changeBackgroundImage);
+
+const selectBlend= document.querySelector('#select-blend')
+
+const changeBlendMode=(event)=>{
+    const button=event.target;
+    const optionBlend=button.getAttribute('data-option-blend');
+    console.log(optionBlend);
+}
+selectBlend.addEventListener('input', changeBlendMode);
+
 
